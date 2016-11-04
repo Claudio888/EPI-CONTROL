661971c6b0f2b0e271e18c3c2ba6d62eac68f108
Claudio Gonçalves Filho <claudio.goncalves.filho@outlook.com>
2016-11-04 11:17:32 -0200

    Update EPICONTROL2.0.py

--- EPICONTROL2.0.py
+++ EPICONTROL2.0.py
@@ -1,30 +1,38 @@
 import xlrd
 import smtplib
-import datetime
 from datetime import datetime
 
-workbook = xlrd.open_workbook('dados.xlsx')
+workbook = xlrd.open_workbook('sua planilha excel com os dados.xlsx') #Define para workbook a função de abertura, designando uma planilha a ele.
 
-worksheet = workbook.sheet_by_index(0)
+worksheet = workbook.sheet_by_index(0) #Define qual woorkbook sera utilizado, qual aba. 
 
-hoje = datetime.today()
+hoje = datetime.today() #designa a data de hoje a variavel hoje
 
 
-for row_num in range(worksheet.nrows):
-    if row_num == 0:
+# O for a seguir atribui os valores que estão na planilha para as variaveis. 
+for row_num in range(worksheet.nrows): 
+    if row_num == 0: 
         continue
-    row = worksheet.row_values(row_num)
-    datemode = workbook.datemode
-    data_recebimento = datetime(*xlrd.xldate_as_tuple(row[2],datemode))
-    data_devolucao = datetime(*xlrd.xldate_as_tuple(row[3], datemode))
-    nome = (row[0])
-    epi = (row[1])
+        row = worksheet.row_values(row_num)
+        datemode = workbook.datemode
+        data_recebimento = datetime(*xlrd.xldate_as_tuple(row[2],datemode)) #a função datetime converte os valores da planilha para data normal xx/xx/xx, possibliitando sua comparação em python
+        data_devolucao = datetime(*xlrd.xldate_as_tuple(row[3], datemode))
+        nome = (row[0])
+        epi = (row[1])
 
-if data_devolucao <= hoje:
-    print(data_devolucao)
-    print("enviar email")
-    print(nome)
+    if data_devolucao <= hoje: #faz a comparação de datas. 
+        print(data_devolucao)
+        print("enviar email")
+        print(nome)
 
+    '''
+    Parte do código para enviar o email atraves de servidor SMTP, no caso utilizo o do gmail, pode se utilizar outro. 
+    
+    Exemplos no site :  http://www.codepianist.com/other/lista-de-servidores-de-email-smtp-pop/
+    
+    
+    ''' 
+    
     smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 
     smtp.login('seu email', 'sua senha')
